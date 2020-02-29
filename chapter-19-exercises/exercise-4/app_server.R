e405462e8022ed27320c39c4d9aad5bfc108577b
Bruno Barreto <brunob42@uw.edu>
2020-02-29 14:18:36 -0800

    Added completed chapter 19 files

--- chapter-19-exercises/exercise-4/app_server.R
+++ chapter-19-exercises/exercise-4/app_server.R
@@ -1,21 +1,18 @@
 # Load libraries so they are available
+library("dplyr")
 library("shiny")
 library("ggplot2")
 
 # Read data file
-income_growth <- read.csv("data/income_growth_1980-2014.csv")
+income_growth <- read.csv("data/income_growth_1980-2014.csv") %>% rename(Income.Percentile = ï..Income.Percentile)
 
 # Define a server function
 server <- function(input, output) {
   output$plot <- renderPlot({
     # return the plot
     ggplot(data = income_growth) +
-      geom_point(mapping = aes(
-        x = Income.Percentile, y = Average.Growth.Perc
-      ), color = "gray") +
-      geom_point(mapping = aes(
-        x = Income.Percentile, y = Post.Tax.Growth.Perc
-      ), color = "red") +
+      geom_point(mapping = aes(x = Income.Percentile, y = Average.Growth.Perc), color = "gray") +
+      geom_point(mapping = aes(x = Income.Percentile, y = Post.Tax.Growth.Perc), color = "red") +
       labs(x = "Income Percentile", y = "Income Growth (%)") +
       scale_x_continuous(limits = input$percentile)
   })
